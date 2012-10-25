913ee1dd9a50fa0dbfb22a5e1266c5f23b390142
David Foster <davidfstr@gmail.com>
2012-10-24 22:56:42 -0700

    Test writing an alias record from scratch.

--- alias_record.py
+++ alias_record.py
@@ -73,34 +73,76 @@ def main(args):
     # which is the primary resource contained in an alias file
     (command, alias_record_file_filepath) = args
     
+    with open(alias_record_file_filepath, 'rb') as input:
+        alias_record = read_alias_record(input)
+    
     if command == 'info':
-        with open(alias_record_file_filepath, 'rb') as input:
-            alias_record = read_alias_record(input)
-        
         print_alias_record(alias_record)
         
-    elif command == 'test':
-        with open(alias_record_file_filepath, 'rb') as input:
-            alias_record = read_alias_record(input)
-        
+    elif command == 'test_read_write':
         output = StringIO()
         write_alias_record(output, **alias_record)
         
-        actual_output = output.getvalue()
-        with open(alias_record_file_filepath, 'rb') as file:
-            expected_output = file.read()
+        verify_matches(output, alias_record_file_filepath, alias_record)
         
-        matches = (actual_output == expected_output)
-        print 'Matches? ' + ('yes' if matches else 'no')
-        if not matches:
-            print '    Expected: ' + repr(expected_output)
-            print '    Actual:   ' + repr(actual_output)
-            print
-            print_alias_record(alias_record)
+    elif command == 'test_write_custom_matching':
+        test_write_custom_matching(alias_record_file_filepath, alias_record)
         
     else:
         sys.exit('Unrecognized command: %s' % command)
         return
+
+
+def test_write_custom_matching(alias_record_file_filepath, alias_record):
+    # "AppAlias.rsrc.dat"
+    output = StringIO()
+    write_alias_record(output,
+        record_size=202,
+        alias_kind=0,
+        volume_name='Boot',
+        volume_created=3431272487,
+        parent_directory_id=542,
+        file_name='app',
+        file_number=543,
+        # NOTE: Can't get file_created reliably from hfsutil CLI
+        file_created=3265652246,
+        file_type='APPL',
+        file_creator='AQt7',
+        nlvl_from=1,
+        nlvl_to=1,
+        extras=[
+            Extra(0, 'parent_directory_name', 'B'),
+            Extra(1, 'directory_ids', [542, 541, 484]),
+            Extra(2, 'absolute_path', 'Boot:AutQuit7:A:B:app'),
+            Extra(0xFFFF, 'end', None)
+        ])
+    
+    """
+    # "AutQuit7 Alias Data.rsrc.dat"
+    output = StringIO()
+    write_alias_record(output,
+        record_size=200,
+        alias_kind=0,
+        volume_name='Boot',
+        volume_created=3431272487,
+        parent_directory_id=484,
+        file_name='AutQuit7',
+        file_number=485,
+        # NOTE: Can't get file_created reliably from hfsutil CLI
+        file_created=3265652246,
+        file_type='APPL',
+        file_creator='AQt7',
+        nlvl_from=1,
+        nlvl_to=1,
+        extras=[
+            Extra(0, 'parent_directory_name', 'AutQuit7'),
+            Extra(1, 'directory_ids', [484]),
+            Extra(2, 'absolute_path', 'Boot:AutQuit7:AutQuit7'),
+            Extra(0xFFFF, 'end', None)
+        ])
+    """
+    
+    verify_matches(output, alias_record_file_filepath, alias_record)
 
 
 def print_alias_record(alias_record):
@@ -115,6 +157,20 @@ def print_alias_record(alias_record):
                 print '    ' + repr(extra)
         else:
             print '%s: %s' % (member.name, repr(value))
+
+
+def verify_matches(output, alias_record_file_filepath, alias_record):
+    actual_output = output.getvalue()
+    with open(alias_record_file_filepath, 'rb') as file:
+        expected_output = file.read()
+    
+    matches = (actual_output == expected_output)
+    print 'Matches? ' + ('yes' if matches else 'no')
+    if not matches:
+        print '    Expected: ' + repr(expected_output)
+        print '    Actual:   ' + repr(actual_output)
+        print
+        print_alias_record(alias_record)
 
 # ------------------------------------------------------------------------------
 
@@ -226,7 +282,7 @@ def write_structure(output, structure_members, **fieldargs):
     for member in structure_members:
         value = fieldargs.get(member.name, member.default_value)
         if value is None:
-            raise ValueError('No value specified for member "%s", which has no default value.' % member.name)
+            raise ValueError('No value specified for member "%s", which lacks a default value.' % member.name)
         this_module['write_' + member.type](output, member.subtype, value)
 
 
